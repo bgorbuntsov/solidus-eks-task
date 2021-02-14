42f04960394aaa280766661755a7de75ade4e28e
Boris Gorbuntsov <gorbuntsov@gmail.com>
2021-02-14 05:40:50 +0200

    Readme edited

--- eks/cluster/setvar.sh
+++ eks/cluster/setvar.sh
@@ -7,7 +7,7 @@ export TF_VAR_domainname='kube.solidus.io'
 # Repo storing apps sourcecode
 export TF_VAR_reponame_prefix='https://github.com/bgorbuntsov/'
 # Github access token (shoul be generated via Github web)
-export TF_VAR_github_token='dc30a9188947902173f924640793540f2b5ef5bc'
+export TF_VAR_github_token='toKeNtoKeNtoKeNtoKeNtoKeNtoKeNtoKeN'
 # AWS region to run all this stuff
 export TF_VAR_region='us-east-1'
 # Database user
@@ -15,6 +15,6 @@ export TF_VAR_rds_user='user'
 # Database name
 export TF_VAR_rds_database='solidusdb'
 # AWS access
-export AWS_ACCESS_KEY_ID="AKIA6QPXQWTPSTTVDLZX"
-export AWS_SECRET_ACCESS_KEY="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"
+export AWS_ACCESS_KEY_ID="acceSsKeyId"
+export AWS_SECRET_ACCESS_KEY="SecreTacceSSKey"
 
